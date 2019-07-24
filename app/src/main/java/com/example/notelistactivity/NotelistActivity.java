c7ea208fbf00722a2873e60e0e45968fc92fa74e
CYNTHIA <njokicynthia590@gmail.com>
2019-07-24 19:13:13 +0300

    Initial commit

---
+++ app/src/main/java/com/example/notelistactivity/NotelistActivity.java
@@ -0,0 +1,54 @@
+package com.example.notelistactivity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.List;
+
+public class NotelistActivity extends AppCompatActivity {
+    private NoteRecyclerAdapter mNoteRecyclerAdapter;
+    RecyclerView recyclerNotes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        recyclerNotes = findViewById(R.id.list_notes);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("NoteKeeper");
+        initializeDisplayContent();
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mNoteRecyclerAdapter.notifyDataSetChanged();
+    }
+
+    private void initializeDisplayContent() {
+        recyclerNotes.setLayoutManager(new LinearLayoutManager(this));
+
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        mNoteRecyclerAdapter = new NoteRecyclerAdapter(this, notes);
+        recyclerNotes.setAdapter(mNoteRecyclerAdapter);
+
+    }
+}
+
